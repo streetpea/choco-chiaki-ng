3ffd01bfecee23a3ae0ea0c8c8db75cb503d39fb
Street Pea <streetpea@proton.me>
2024-11-11 13:34:49 -0800

    Initial chiaki-ng chocolatey package scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chocolatey chiaki-ng package
+
+This repo holds the chocolatey package materials for `chiaki-ng`.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://github.com/streetpea/chiaki-ng/releases/download/v1.9.1/chiaki-ng-windows-installer.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'chiaki-ng'
+
+  checksum      = '42B9AE0FAB026BC8387C0345BDA04EA17ABE24340A89699D75ECCFCBE7712D1F'
+  checksumType  = 'sha256'
+  checksum64    = '42B9AE0FAB026BC8387C0345BDA04EA17ABE24340A89699D75ECCFCBE7712D1F'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
